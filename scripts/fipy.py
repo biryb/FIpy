9a56ee847212619804330cd88b94df9f06af5be3
Birgitta Ryback <birgittaryback@gmail.com>
2025-02-04 17:19:47 -0500

    changed the output directory to root mzml

--- scripts/fipy.py
+++ scripts/fipy.py
@@ -17,8 +17,6 @@ def main():
     # Set up argument parser
     parser = argparse.ArgumentParser(description='Process mzML files and perform analysis')
     parser.add_argument('dir_mzml', help='Directory containing mzML files')
-    parser.add_argument('dir_analysis', help='Directory to save analysis results')
-
     # Parse the arguments
     args = parser.parse_args()
 
@@ -26,13 +24,14 @@ def main():
     init_start_time = time.time()
     os.chdir(args.dir_mzml)
     list_mzml_files = os.listdir(args.dir_mzml)
+    dir_analysis = args.dir_mzml
     
     # Read in files and generate a dictionary with all the data
     print('Reading .mzML files')
     dict_all_rawdata = read_mzml_files(list_mzml_files)
     print('Merging .mzML files')
     start_time = time.time()
-    dict_consensus_data = merge_mz_int_file(dict_all_rawdata, tolerance=0.001)
+    dict_consensus_data = merge_mz_int_file(dict_all_rawdata, tolerance=0.005)
     elapsed_time = time.time() - start_time
     print(f"Merging m/z's within each file took {elapsed_time/60:.4f} minutes to run.")
     
@@ -47,21 +46,23 @@ def main():
     df_merged = merge_files(df)
     
     elapsed_time = time.time() - start_time
-    print(f"Merging m/z's between files took {elapsed_time:.4f} seconds to run.")
+    print(f"Merging m/z's between files took {elapsed_time/60:.4f} minutes to run.")
     
     print('Filtering out ions present in less than 50% of the samples')
     df_filtered = filter_rare_ions(df_merged)
     
-    output_dir = os.path.join(args.dir_analysis, "fipy_output")
+    output_dir = os.path.join(dir_analysis, "fipy_output")
     
     # Create the folder if it doesn't exist
     try:  
         os.mkdir(output_dir)
+        order_number = 1
     except OSError as error:  
-        print(error)
+        print("Folder exist, appending to existing folder")
+        order_number = len(os.listdir(output_dir))+1
 
     print(f"Saving results in: {output_dir}")        
-    df_filtered.to_excel(os.path.join(output_dir, "raw_output.xlsx"))
+    df_filtered.to_excel(os.path.join(output_dir, f"raw_output_{order_number}.xlsx"))
     elapsed_time = time.time() - init_start_time
     print(f"Finished and took {elapsed_time/60:.4f} minutes to run.")
 
